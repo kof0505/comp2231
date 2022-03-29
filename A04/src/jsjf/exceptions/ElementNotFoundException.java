e6ed05aa78ab60a3025287fed4df776f8bea9bcd
Min Kim <85675693+kof0505@users.noreply.github.com>
2022-03-28 19:18:33 -0700

    Delete A04 directory

--- A04/src/jsjf/exceptions/ElementNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package jsjf.exceptions;
-import jsjf.exceptions.*;
-public class ElementNotFoundException extends RuntimeException {
-
-    public ElementNotFoundException(String collection) {
-
-        super("The " + collection + " is empty.");
-    }
-}
--- A04/src/jsjf/exceptions/NonComparableElementException.java
+++
@@ -1,9 +0,0 @@
-package jsjf.exceptions;
-import jsjf.exceptions.*;
-public class NonComparableElementException extends RuntimeException {
-
-    public NonComparableElementException(String collection) {
-
-        super("The " + collection + " is empty.");
-    }
-}
